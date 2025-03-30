6a20ad1d96f497e9152753c5a26aa468097ca8e7
-3σ <yurippe@m3sigma.net>
2025-03-30 19:39:55 +0900

    -

--- init.lua
+++ init.lua
@@ -1,13 +1,5 @@
 local vim = vim
 
-local data_dir = vim.fn.stdpath('data')
-if vim.fn.empty(vim.fn.glob(data_dir .. '/site/autoload/plug.vim')) == 1 then
-	vim.cmd('silent !curl -fLo ' ..
-		data_dir ..
-		'/site/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim')
-	vim.cmd('autocmd VimEnter * PlugInstall --sync | source $MYVIMRC')
-end
-
 local Plug = vim.fn['plug#']
 
 vim.g.loaded_netrw = 1
